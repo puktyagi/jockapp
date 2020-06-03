653fab37c58c21b0f2529dcce61304498e18405b
puktyagi <H@riom12>
2020-06-03 23:11:29 +0530

    Setting project,"First Commit"

---
+++ src/main/java/shishayspringframework/jockapp/JockappApplication.java
@@ -0,0 +1,13 @@
+package shishayspringframework.jockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JockappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JockappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shishayspringframework/jockapp/JockappApplicationTests.java
@@ -0,0 +1,13 @@
+package shishayspringframework.jockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JockappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
