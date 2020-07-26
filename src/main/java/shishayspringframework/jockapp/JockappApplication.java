db8047b75d4e1fc475d2b529382d7273b63a8783
puktyagi <puktyagi@gmail.com>
2020-07-26 22:45:46 +0530

    @configuration using java

--- src/main/java/shishayspringframework/jockapp/JockappApplication.java
+++ src/main/java/shishayspringframework/jockapp/JockappApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import shishayspringframework.jockapp.controller.JockController;
 
 import javax.naming.Context;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"config", "shishayspringframework.jockapp"})
 public class JockappApplication {
 
     public static void main(String[] args) {
--- src/main/java/shishayspringframework/jockapp/service/ChuckNorrisJockService.java
+++ src/main/java/shishayspringframework/jockapp/service/ChuckNorrisJockService.java
@@ -1,17 +1,18 @@
 package shishayspringframework.jockapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import shishayspringframework.jockapp.model.Jock;
 
 @Service
 public class ChuckNorrisJockService implements JockService{
 
+    @Autowired
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    ChuckNorrisJockService()
-    {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public ChuckNorrisJockService(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
